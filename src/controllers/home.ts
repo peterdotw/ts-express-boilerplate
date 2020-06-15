654e8446ba27be32d88be695ec37464fb7cfa4b1
peterdotw <piotrwiderapraca@gmail.com>
2020-06-15 18:19:58 +0200

    feat: add few types

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
-export const index = (_: Request, res: Response) => {
+export const index = (_: Request, res: Response): void => {
   res.send("Hi from TypeScript!");
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = 3001;
+const port: number = 3001;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
