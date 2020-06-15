75677c99c8c0e6f3ca65ed4cc3a7ec9b46f489f2
peterdotw <piotrwiderapraca@gmail.com>
2020-06-15 02:08:19 +0200

    feat: add Helmet and change test scripts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import helmet from "helmet";
 
 import * as homeController from "./controllers/home";
 
 const app = express();
 
+app.use(helmet());
 app.get("/", homeController.index);
 
 export default app;
--- test/home.test.ts
+++ test/home.test.ts
@@ -2,11 +2,11 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("GET /", () => {
-  it("should return 200 OK", (done) => {
+  it("should return 200 OK", async (done) => {
     request(app).get("/").expect(200, done);
   });
 
-  it("should return matching body", (done) => {
+  it("should return matching body", async (done) => {
     request(app).get("/").expect("Hi from TypeScript!", done);
   });
 });
